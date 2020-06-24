2f44d83a223240be8ea189baa677bd1d1dc18460
Hyun Ahn <codepawn@icloud.com>
2020-06-24 12:49:30 +0900

    add test buttons

--- doodle/classes.dart
+++ doodle/classes.dart
@@ -1,12 +1,41 @@
 abstract class Item {
-  use()
+  use();
 }
 
-class Chest<T> implements Item{
+class Chest<T> implements Item {
   List<T> contents;
+
+  Chest(this.contents);
+
+  use() => print("$this has ${contents.length} items.");
 }
 
-main(){
+class Sword implements Item {
+  int damage = 5;
+
+  use() => print("$this dealt $damage damage.");
+}
+
+class DiamondSword extends Sword {
+  int damage = 50;
+}
+
+class IronSword extends Sword {
+  int quantity = 2;
+}
+
+main() {
   var chest = Chest<Item>([
+    DiamondSword(),
+    Sword(),
   ]);
+
+  chest.use();
+
+  for (var item in chest.contents) {
+    item.use();
+  }
+
+  var is0 = new IronSword();
+  print(is0.quantity);
 }
--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -1,48 +1,109 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(new MyApp());
 }
 
 class MyApp extends StatefulWidget {
   @override
-  int counter = 0;
+  _MyAppState createState() => _MyAppState();
+}
 
-  void increaseCounter(){
-    counter++;
+class _MyAppState extends State<MyApp> {
+  int counter = 0; // Text UI에 표시될 값
+  void increaseCounter() {
+    // 버튼을 누르면 counter 값을 증가시킴
+    setState(() {
+      counter += 1;
+    });
   }
 
-  Widget build(BuildContext context){
+  void decreaseCounter() {
+    // 버튼을 누르면 counter 값을 감소시킴
+    setState(() {
+      counter -= 1;
+    });
+  }
+
+  void timesCounter() {
+    setState(() {
+      counter *= 2;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return MaterialApp(
       title: "첫 플러터 앱",
       home: Scaffold(
-        appBar:AppBar(
-          title:Text("내 플러터 앱"),
+        appBar: AppBar(
+          title: Text("내 플러터 앱"),
         ),
-        body:Container(
+        body: Container(
+          padding: const EdgeInsets.all(16.0), // 전체적으로 16씩 패딩을 주었음.
           child: Column(
+            // 세로로 위젯을 배치
             children: <Widget>[
-              Text("$counter"),
-              RaisedButton(
-                child: Text(
-                  "더행 $counter",
-                  style: TextStyle(color: Colors.white),
-                ),
-                color:Colors.red,
-                onPressed: (){
-                  increaseCounter();
-                }
+              Text(
+                "$counter",
+                style: TextStyle(fontSize: 160.0),
+              ),
+              Row(
+                // 가로로 위젯을 배치, 안에 버튼 2개가 들어가 있음.
+                mainAxisAlignment:
+                    MainAxisAlignment.spaceEvenly, // 위젯끼리 같은 공간만큼 띄움
+                children: <Widget>[
+                  RaisedButton(
+                    child: Text(
+                      "Add",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    color: Colors.red, // 버튼 색은 빨강색으로
+                    onPressed: () {
+                      // 버튼을 누르면 안에 있는 함수를 실행
+                      increaseCounter();
+                    },
+                  ),
+                  RaisedButton(
+                    child: Text(
+                      "Subtract",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    color: Colors.blue, // 버튼 색은 파란색으로
+                    onPressed: () {
+                      // 버튼을 누르면 안에 있는 함수를 실행
+                      decreaseCounter();
+                    },
+                  ),
+                  RaisedButton(
+                      child: Text(
+                        "Times",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      color: Colors.amber,
+                      onPressed: () {
+                        timesCounter();
+                      }
+                  ),
+                  RaisedButton(
+                      child: Text(
+                        "Times",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      color: Colors.amberAccent,
+                      onPressed: () {
+                        timesCounter();
+                      }
+                  ),
+                ],
               )
-            ]
-          )
-          
-        )
-      )
+            ],
+          ),
+        ),
+      ),
     );
   }
-
 }
-
 // class MyApp extends StatelessWidget {
 //   // This widget is the root of your application.
 //   @override
@@ -160,7 +221,6 @@ class MyApp extends StatefulWidget {
 //         tooltip: 'Increment',
 //         child: Icon(Icons.add),
 //       ), // This trailing comma makes auto-formatting nicer for build methods.
-
 
 //     );
 //   }
