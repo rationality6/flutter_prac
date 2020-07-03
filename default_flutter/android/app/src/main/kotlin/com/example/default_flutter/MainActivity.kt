c0808bdc0620b46cf381e80158283ffd5dce2cde
Hyun Ahn <codepawn@icloud.com>
2020-07-03 14:48:01 +0900

    정리

--- default_flutter/android/app/src/main/kotlin/com/example/default_flutter/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.default_flutter
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- hello_world/lib/main.dart
+++ hello_world/lib/main.dart
@@ -9,6 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demooo',
       theme: ThemeData(
         primarySwatch: Colors.red,
@@ -27,102 +28,74 @@ class RandomWords extends StatefulWidget {
 }
 
 class _RandomWordsState extends State<RandomWords> {
-  int _counter0 = 1;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter0 += 1;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
-
-    return Scaffold(
-      appBar: AppBar(title: Text(widget.title)),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[Text(wordPair.asPascalCase), Text('$_counter0')],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        // child: Icon(Icons.add),
-        child: Text("foo"),
-      ),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
-  void _incrementCounter() {
+  void _incrementCounter(num) {
     setState(() {
-      _counter += 1;
+      _counter += num;
+    });
+  }
+
+  void _timesCounter() {
+    setState(() {
+      _counter *= 9;
     });
   }
 
   @override
   Widget build(BuildContext context) {
     final wordPair = WordPair.random();
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(wordPair.asPascalCase),
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(wordPair.asPascalCase),
+              Text('$_counter'),
+              Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                RaisedButton(
+                    child: Text("1"),
+                    onPressed: () {
+                      _incrementCounter(1);
+                    }),
+                RaisedButton(
+                    child: Text("2"),
+                    onPressed: () {
+                      _incrementCounter(2);
+                    }),
+                RaisedButton(
+                    child: Text("3"),
+                    onPressed: () {
+                      _incrementCounter(3);
+                    }),
+              ]),
+              Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                RaisedButton(
+                    child: Text("1"),
+                    onPressed: () {
+                      _incrementCounter(1);
+                    }),
+                RaisedButton(
+                    child: Text("2"),
+                    onPressed: () {
+                      _incrementCounter(2);
+                    }),
+                RaisedButton(
+                    child: Text("3"),
+                    onPressed: () {
+                      _incrementCounter(3);
+                    }),
+              ])
+            ]),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        onPressed: () {
+          _timesCounter();
+        },
+        // child: Icon(Icons.add),
+        child: Text("*9"),
+      ),
     );
   }
 }
